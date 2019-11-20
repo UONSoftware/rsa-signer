82b6054278b3d0b88ef2d2f1ff68fc55ccdf0141
Dusan Malusev <dusan.998@outlook.com>
2019-11-20 11:26:05 +0100

    Making service laravel independent

--- src/RsaSignerServiceProvider.php
+++ src/RsaSignerServiceProvider.php
@@ -6,17 +6,28 @@ namespace UonSoftware\RsaSigner;
 
 use UonSoftware\RsaSigner\Services\RsaSigner;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Foundation\Application;
 use UonSoftware\RsaSigner\Contracts\RsaSigner as Contract;
 
 class RsaSignerServiceProvider extends ServiceProvider
 {
-
+    
     public function register(): void
     {
         $this->mergeConfigFrom(__DIR__.'/../config/rsa.php', 'rsa');
-        $this->app->singleton(Contract::class, RsaSigner::class);
-    }
+        $this->app->singleton(Contract::class, function (Application $app) {
 
+            /** @var \Illuminate\Config\Repository $config */
+            $config = $app['config'];
+            
+            return new RsaSigner(
+                $config->get('rsa.private'),
+                $config->get('rsa.public'),
+                $config->get('rsa.password')
+            );
+        });
+    }
+    
     public function boot(): void
     {
         $this->publishes([
--- src/Services/RsaSigner.php
+++ src/Services/RsaSigner.php
@@ -9,7 +9,6 @@ use UonSoftware\RsaSigner\Exceptions\PrivateKeyError;
 use UonSoftware\RsaSigner\Exceptions\SignatureCorrupted;
 use UonSoftware\RsaSigner\Exceptions\TokenSignatureInvalid;
 use UonSoftware\RsaSigner\Contracts\RsaSigner as Contract;
-use Illuminate\Contracts\Config\Repository as Config;
 
 /**
  * Class RsaSigner
@@ -22,24 +21,22 @@ class RsaSigner implements Contract
      * @var false|resource
      */
     private $publicKey;
-
+    
     /**
      * @var false|resource
      */
     private $privateKey;
-
-
+    
+    
     /**
      * RsaSigner constructor.
-     * @param Config $config
-     * @throws PublicKeyError
-     * @throws PrivateKeyError
+     *
+     * @param  string  $privateKey
+     * @param  string  $publicKey
+     * @param  string  $passphrase
      */
-    public function __construct(Config $config)
+    public function __construct(string $privateKey, string $publicKey, string $passphrase)
     {
-        $publicKey = $config->get('rsa.public');
-        $privateKey = $config->get('rsa.private');
-        $passphrase = $config->get('rsa.passphrase');
         if (!($this->publicKey = openssl_pkey_get_public($publicKey))) {
             throw new PublicKeyError(openssl_error_string());
         }
@@ -47,10 +44,10 @@ class RsaSigner implements Contract
             throw new PrivateKeyError(openssl_error_string());
         }
     }
-
+    
     /**
-     * @param string $data
-     * @param int $algorithm
+     * @param  string  $data
+     * @param  int  $algorithm
      *
      * @return string
      * @throws SignatureCorrupted
@@ -63,11 +60,11 @@ class RsaSigner implements Contract
         }
         return bin2hex($signature);
     }
-
+    
     /**
-     * @param string $data
-     * @param string $signature
-     * @param int $algorithm
+     * @param  string  $data
+     * @param  string  $signature
+     * @param  int  $algorithm
      *
      * @return string
      * @throws TokenSignatureInvalid
@@ -75,7 +72,7 @@ class RsaSigner implements Contract
     public function verify(string $data, string $signature, $algorithm = OPENSSL_ALGO_SHA512): string
     {
         $isValid = openssl_verify($data, hex2bin($signature), $this->publicKey, $algorithm);
-
+        
         if ($isValid === -1 || $isValid === 0) {
             throw new TokenSignatureInvalid();
         }
