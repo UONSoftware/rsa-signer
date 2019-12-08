62316415056a579a98985202f89846ca024dc324
Dusan Malusev <dusan.998@outlook.com>
2019-12-08 17:48:02 +0100

    commenting rsa config file

--- config/rsa.php
+++ config/rsa.php
@@ -1,7 +1,37 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Public Key
+    |--------------------------------------------------------------------------
+    |
+    | A path or resource to your public key.
+    |
+    | E.g. 'file://path/to/public/key'
+    |
+    */
     'public'   => 'file://' . __DIR__ . '/../keys/public.pem',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Private Key
+    |--------------------------------------------------------------------------
+    |
+    | A path or resource to your private key.
+    |
+    | E.g. 'file://path/to/private/key'
+    |
+    */
     'private'  => 'file://' . __DIR__ . '/../keys/private.pem',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Passphrase
+    |--------------------------------------------------------------------------
+    |
+    | The passphrase for your private key. Can be null if none set.
+    |
+    */
     'password' => env('RSA_PRIVATE_KEY_PASSWORD', null),
 ];
